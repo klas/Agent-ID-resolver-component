63a30771a8c3dc320045f5c49fb5b35371ae26c3
Klas Berlic Fras <klas@k-k-k.biz>
2024-08-23 16:45:01 +0200

    implement first resolving attempt

--- app/Strategy/VnrStepFilteringResolvingStrategy.php
+++ app/Strategy/VnrStepFilteringResolvingStrategy.php
@@ -4,8 +4,10 @@ namespace App\Strategy;
 
 use App\Builder\StepFilterBuilderInterface;
 use App\DTO\MaklerDTO;
+use App\Models\Geselschaft;
 use App\Strategy\VnrResolvingStrategyInterface;
 use Illuminate\Support\Facades\App;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class VnrStepFilteringResolvingStrategy implements VnrResolvingStrategyInterface
 {
@@ -15,32 +17,52 @@ class VnrStepFilteringResolvingStrategy implements VnrResolvingStrategyInterface
     public function resolve(array $data = []): ?MaklerDTO
     {
         $filteredVnr = $this->filterVnr($data['vnr'], $data['geselschaft']);
+        $geselschaft = Geselschaft::whereName($data['geselschaft'])->with('maklers')->first();
+
+        // Try to match with stored aliases
+        $maklers = $geselschaft->maklers;
+
+        $searchableAliases = collect([]);
+
+        $geselschaft->maklers->each(function($makler) use($searchableAliases) {
+            $searchableAliases = $searchableAliases->merge($makler->pivot->vnraliases);
+        });
+
+        $makler = $searchableAliases->firstWhere('name', '==', $filteredVnr)?->geselschafts_makler->makler;
+
+
+
+        // No 100% match, try matching filtered stored aliases
+
+        // Still no match, try searching
+
+
+        if ($makler) {
+            return new MaklerDTO($makler->name);
+        }
 
         return null;
     }
 
-    protected function filterVnr(string $filterable, string $geselschaft)
+    protected function filterVnr(string $filterable, string $geselschaft): ?string
     {
         $filterDefinitons = App::tagged('filter_definition');
 
-        $filters = [];
-
         $this->stepFilterBuilder->setFilterable($filterable);
+        $handlerFound = false;
 
         foreach ($filterDefinitons AS $filterDefiniton) {
-            if ($filterDefiniton->responsible($geselschaft))
+            if ($handlerFound = $filterDefiniton->responsible($geselschaft))
             {
                 $filterDefiniton->setStepFilterBuilder($this->stepFilterBuilder);
                 $filterDefiniton->filterChain();
             }
         }
 
-
-
-        foreach ($filters AS $filter) {
-
+        if (!$handlerFound) {
+            throw new NotFoundHttpException('Filter Definition Not Found');
         }
 
-        return $filtered;
+        return $this->stepFilterBuilder->getFiltered();
     }
 }
