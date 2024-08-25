453f3ac7fea6a5e6f754f6c6ccdadc7402e5efb5
Klas Berlic Fras <klas@k-k-k.biz>
2024-08-25 17:46:08 +0200

    improve logic

--- app/Strategy/VnrStepFilteringResolvingStrategy.php
+++ app/Strategy/VnrStepFilteringResolvingStrategy.php
@@ -5,6 +5,7 @@ namespace App\Strategy;
 use App\Builder\StepFilterBuilderInterface;
 use App\DTO\MaklerDTO;
 use App\Models\Geselschaft;
+use App\Models\GeselschaftsMakler;
 use App\Strategy\VnrResolvingStrategyInterface;
 use Illuminate\Support\Facades\App;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -30,12 +31,26 @@ class VnrStepFilteringResolvingStrategy implements VnrResolvingStrategyInterface
         // No 100% match, try matching filtered value
         if (!$makler) {
             $filteredVnr = $this->filterVnr($data['vnr'], $data['geselschaft']);
-            $makler = $searchableAliases->firstWhere('name', '==', $filteredVnr)?->geselschafts_makler->makler;
+            if($makler = $searchableAliases->firstWhere('name', '==', $filteredVnr)
+                ?->geselschafts_makler->makler)
+            {
+                $makler->pivot->vnraliases()->create(['name' => $data['vnr'], 'gm_id' => $makler->pivot->id]);
+            };
         }
 
-        // Still no match, try matching filtered stored aliases with filtered value, if match store it for next time
-        //$makler = $searchableAliases->firstWhere('name', '==', $filteredVnr)?->geselschafts_makler->makler;
-
+        // Still no match, try matching filtered stored aliases with filtered value,
+        // if match store unfiltered and filtered value for next time
+        if (!$makler) {
+            $searchableAliases->each(function($alias) use(&$makler, $filteredVnr, $data) {
+                if($filteredVnr === $this->filterVnr($alias->name, $data['geselschaft']))
+                {
+                    $makler = $alias?->geselschafts_makler->makler;
+                    $makler->pivot->vnraliases()->create(['name' => $data['vnr'], 'gm_id' => $makler->pivot->id]);
+                    $makler->pivot->vnraliases()->create(['name' => $filteredVnr, 'gm_id' => $makler->pivot->id]);
+                    return false; // break loop
+                };
+            });
+        }
 
         if ($makler) {
             return new MaklerDTO($makler->name);
