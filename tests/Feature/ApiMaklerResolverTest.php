763c3435eb38edd4c49b0c39e68ba21b0a123fce
Klas Berlic Fras <klas@k-k-k.biz>
2024-08-25 23:52:04 +0200

    fix test

--- tests/Feature/ApiMaklerResolverTest.php
+++ tests/Feature/ApiMaklerResolverTest.php
@@ -13,17 +13,52 @@ use Tests\TestCase;
 
 class ApiMaklerResolverTest extends TestCase
 {
+    private const BASIC_URL = "http://localhost/api/makler";
+
     public function testShowReturnsValidData()
     {
-        $vnr = '00654564';
-        $geselchaft = 'Haftpflichtkasse Darmstadt';
+        $maklers['max'] = 'Max Mustermann';
+        $maklers['not_max'] = 'Not Max';
 
-        $response = $this->get("http://localhost/api/makler?vnr=$vnr&geselschaft=$geselchaft");
+        $geselschaftsVnrs['max'] = [
+            'Haftpflichtkasse Darmstadt' => ['00654564', '654564', '654-564'],
+            'WWK' => ['Q412548787', '412548787'],
+            'Axa Versicherung' => ['15154184714-000', '15154184714', '99/15154184714'],
+            'Ideal Versicherung' => ['006674BA23', '6674BA23', '6674-BA23'],
+            'die Bayerische' => ['54501R784', '54501-R784', '54501784'],
+        ];
 
-        $response->assertStatus(Response::HTTP_OK);
+        $geselschaftsVnrs['not_max'] = [
+            'Haftpflichtkasse Darmstadt' => ['00654574', '654574', '654-574'],
+            'WWK' => ['Q412548777', '412548777'],
+            'Axa Versicherung' => ['15154184774-000', '15154184774', '99/15154184774'],
+            'Ideal Versicherung' => ['006674BA73', '6674BA73', '6674-BA73'],
+            'die Bayerische' => ['54501R774', '54501-R774', '54501774'],
+        ];
+
+        foreach ($geselschaftsVnrs AS $key => $maklerVnrs) {
+            foreach ($maklerVnrs AS $geselschaft => $vnrs) {
+                foreach ($vnrs AS $vnr) {
+                    $response = $this->get(self::BASIC_URL . "?vnr=$vnr&geselschaft=$geselschaft");
+
+                    $response->assertStatus(Response::HTTP_OK);
+                    $response->assertJson(
+                        [
+                            'name' => $maklers[$key]
+                        ]
+                    );
+                }
+            }
+        }
+    }
+
+    public function testShowReturnsErrorOnInvalidData() {
+        $response = $this->get(self::BASIC_URL . "?vnr=00654564&geselschaft=abc", ['Accept' =>'application/json']);
+
+        $response->assertStatus(Response::HTTP_NOT_FOUND);
         $response->assertJson(
             [
-                'name' => 'Max Mustermann'
+                'message' => 'No query results for model [App\\Models\\Geselschaft].'
             ]
         );
     }
